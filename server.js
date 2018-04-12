27b0b9465d9735199662dcbe9626bf19c5cc71ee
ropas14 <37495035+ropas14@users.noreply.github.com>
2018-04-12 17:50:55 +0200

    Add files via upload
    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ Facials = require('./saveCleansers');
 
 app.use(bodyParser.json());
 
-app.get('/products', function(req, res) {
+app.get('/products', function(req, res) {		
+   res.header("Access-Control-Allow-Origin", "*");  
+   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,'DELETE");  
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, sender, session, session_token, socket");
    Facials.getAllCleansers(function(err, facecleansers) {
       if (err) throw err;
       res.json(facecleansers);
